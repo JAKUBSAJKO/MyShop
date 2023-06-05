9b05723e162c90f1f3eb2b72e69788be8e9b743e
Jakub Sajko <jakubsajko.inf@gmail.com>
2023-06-05 13:52:03 +0200

    Fix problem related to font - our fonts should work across the app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,19 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
+import { Raleway, Roboto } from "next/font/google";
+
+const raleway = Raleway({
+  weight: ["300", "400", "500", "600", "700", "900"],
+  subsets: ["latin"],
+  variable: "--font-raleway",
+});
+
+const roboto = Roboto({
+  weight: ["300", "400", "500", "700", "900"],
+  subsets: ["latin"],
+  variable: "--font-roboto",
+});
 
 export default function App({
   Component,
@@ -14,7 +27,9 @@ export default function App({
     <QueryClientProvider client={queryClient}>
       <Hydrate state={pageProps.dehydratedState}>
         <SessionProvider session={session}>
-          <Component {...pageProps} />
+          <main className={`${raleway.variable} ${roboto.variable}`}>
+            <Component {...pageProps} />
+          </main>
         </SessionProvider>
       </Hydrate>
     </QueryClientProvider>
