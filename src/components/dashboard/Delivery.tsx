8d223d8b015e23ba702e7f93f6c4fb545d183eba
Jakub Sajko <jakub.s@futuresight.com>
2023-09-18 17:01:23 +0200

    Add update quantity function to the delivery page

--- src/components/dashboard/Delivery.tsx
+++ src/components/dashboard/Delivery.tsx
@@ -1,5 +1,8 @@
 import { FormEvent, useEffect, useState } from "react";
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+import { useMutation, useQueryClient } from "react-query";
+import { ClipLoader } from "react-spinners";
+import { updateQuantity } from "../../../services/services";
 import { Product } from "../../../types";
 
 interface DeliveryProps {
@@ -10,6 +13,8 @@ export default function Delivery({ products }: DeliveryProps) {
   const [displayingProduct, setDisplayingProduct] = useState<Product>();
   const [currentProductNumber, setCurrentProductNumber] = useState(0);
   const [quantity, setQuantity] = useState(0);
+
+  const queryClient = useQueryClient();
 
   const prevProduct = () => {
     if (currentProductNumber === 0) {
@@ -27,61 +32,90 @@ export default function Delivery({ products }: DeliveryProps) {
     }
   };
 
+  const { mutate, isLoading } = useMutation(updateQuantity, {
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["products"] });
+    },
+  });
+
   const enterDelivery = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    console.log(quantity);
+    updateQuantityInDB();
+
+    setQuantity(0);
+  };
+
+  const updateQuantityInDB = async () => {
+    if (displayingProduct) {
+      const currentQuantity: number = displayingProduct.quantity + quantity;
+      mutate({ productId: displayingProduct.id, currentQuantity });
+    }
   };
 
   useEffect(() => {
     if (products !== undefined) {
       setDisplayingProduct(products[currentProductNumber]);
     }
-  }, [currentProductNumber]);
+  }, [currentProductNumber, products]);
 
   return (
     <div className="text-white my-16 flex justify-center">
       <div className="flex flex-col items-center gap-12">
-        <div className="flex items-center gap-8">
-          <button onClick={prevProduct} className="w-16 h-16 bg-gray-800 rounded-lg flex justify-center items-center">
-            <FaArrowLeft className="text-2xl" />
-          </button>
-          <div className="bg-gray-800 rounded-2xl p-10 flex flex-col items-center gap-6">
-            <img
-              src={displayingProduct?.image!}
-              alt={displayingProduct?.name!}
-              width={160}
-              height={160}
-              className="rounded-lg"
-            />
-            <h1 className="text-2xl font-medium">{displayingProduct?.name}</h1>
-            <div className="flex flex-col items-center">
-              <p className="font-raleway uppercase">Stan w magazynie</p>
-              <p className="font-raleway text-xl">{displayingProduct?.quantity}</p>
-            </div>
+        {isLoading ? (
+          <div className="h-[500px] flex justify-center items-center">
+            <ClipLoader size={64} color="#ffffff" />
           </div>
-          <button onClick={nextProduct} className="w-16 h-16 bg-gray-800 rounded-lg flex justify-center items-center">
-            <FaArrowRight className="text-2xl" />
-          </button>
-        </div>
-        <form onSubmit={enterDelivery} className="flex items-center">
-          <div className="flex items-center gap-4">
-            <div className="relative w-48 h-20 bg-gray-800 rounded-lg">
-              <input
-                type="number"
-                placeholder="0"
-                min="0"
-                className="w-full h-full px-12 text-center bg-gray-800 rounded-lg text-2xl font-medium placeholder:text-white"
-                value={quantity}
-                onChange={(e) => setQuantity(parseInt(e.target.value))}
-              />
-              <p className="absolute top-2 left-2">Sztuk</p>
+        ) : (
+          <>
+            <div className="flex items-center gap-8">
+              <button
+                onClick={prevProduct}
+                className="w-16 h-16 bg-gray-800 rounded-lg flex justify-center items-center"
+              >
+                <FaArrowLeft className="text-2xl" />
+              </button>
+              <div className="bg-gray-800 rounded-2xl p-10 flex flex-col items-center gap-6">
+                <img
+                  src={displayingProduct?.image!}
+                  alt={displayingProduct?.name!}
+                  width={160}
+                  height={160}
+                  className="rounded-lg"
+                />
+                <h1 className="text-2xl font-medium">{displayingProduct?.name}</h1>
+                <div className="flex flex-col items-center">
+                  <p className="font-raleway uppercase">Stan w magazynie</p>
+                  <p className="font-raleway text-xl">{displayingProduct?.quantity}</p>
+                </div>
+              </div>
+              <button
+                onClick={nextProduct}
+                className="w-16 h-16 bg-gray-800 rounded-lg flex justify-center items-center"
+              >
+                <FaArrowRight className="text-2xl" />
+              </button>
             </div>
-            <button className="w-10 h-10 bg-gray-800 rounded-full flex justify-center items-center">
-              <p className="text-2xl">+</p>
-            </button>
-          </div>
-        </form>
+            <form onSubmit={enterDelivery} className="flex items-center">
+              <div className="flex items-center gap-4">
+                <div className="relative w-48 h-20 bg-gray-800 rounded-lg">
+                  <input
+                    type="number"
+                    placeholder="0"
+                    min="0"
+                    className="w-full h-full px-12 text-center bg-gray-800 rounded-lg text-2xl font-medium placeholder:text-white"
+                    value={quantity}
+                    onChange={(e) => setQuantity(parseInt(e.target.value))}
+                  />
+                  <p className="absolute top-2 left-2">Sztuk</p>
+                </div>
+                <button className="w-10 h-10 bg-gray-800 rounded-full flex justify-center items-center">
+                  <p className="text-2xl">+</p>
+                </button>
+              </div>
+            </form>
+          </>
+        )}
       </div>
     </div>
   );
