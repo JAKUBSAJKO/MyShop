4d3e59c9aca9e0c2a82370fa649f0f1cc519f0b2
JAKUBSAJKO <jakubsajko.inf@gmail.com>
2023-09-26 16:29:19 +0200

    Mobile: adjust success page style

--- src/pages/checkout/success.tsx
+++ src/pages/checkout/success.tsx
@@ -15,16 +15,22 @@ export default function Success() {
 
   return (
     <div className="relative w-full h-screen flex flex-col justify-center items-center gap-4">
-      <h1 className="font-raleway text-6xl">Dziękujemy za Zakupy!</h1>
-      <Image src="/images/delivery.png" alt="" width={360} height={360} />
-      <div className="flex flex-col items-center">
-        <p className="font-raleway font-semibold">
+      <h1 className="font-raleway text-2xl md:text-6xl">Dziękujemy za Zakupy!</h1>
+      <Image
+        src="/images/delivery.png"
+        alt=""
+        width={360}
+        height={360}
+        className="w-56 h-56 my-4 md:my-0 md:w-80 md:h-80"
+      />
+      <div className="max-w-xs flex flex-col items-center text-center px-4 md:max-w-2xl md:px-0 lg:max-w-none lg:text-left">
+        <p className="font-raleway font-semibold text-sm md:text-base">
           Dziękujemy za złożenie zamówienia w naszym sklepie. Jesteśmy zachwyceni, że jesteście naszymi klientami!
         </p>
-        <p className="font-raleway font-semibold">
+        <p className="font-raleway font-semibold text-sm md:text-base">
           Twoja paczka jest w drodze do Ciebie, a my dbamy o to, aby dotarła w idealnym stanie.
         </p>
-        <p className="font-raleway font-semibold">
+        <p className="font-raleway font-semibold text-sm md:text-base">
           Mamy nadzieję, że nasze produkty spełnią Twoje oczekiwania. Zapraszamy ponownie na naszą stronę w przyszłości!
         </p>
       </div>
