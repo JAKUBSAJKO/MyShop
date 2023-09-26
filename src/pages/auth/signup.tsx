46fade3eef0f91d5274ec431d98b89e11e1b2cfa
JAKUBSAJKO <jakubsajko.inf@gmail.com>
2023-09-26 16:13:41 +0200

    Mobile: adjust sign up style

--- src/pages/auth/signup.tsx
+++ src/pages/auth/signup.tsx
@@ -7,9 +7,9 @@ import SignUpForm from "@/components/SignupForm";
 export default function SignUp() {
   return (
     <div className="w-full min-h-screen flex">
-      <div className="flex-1 bg-signup-foto bg-cover bg-no-repeat bg-center"></div>
+      <div className="hidden lg:block flex-1 bg-signup-foto bg-cover bg-no-repeat bg-center"></div>
       <div className="flex-1 bg-nav-grey-200 text-white flex flex-col justify-center items-center">
-        <div className="w-96">
+        <div className="w-72 sm:w-96">
           <h1 className="font-bold text-4xl mb-12">MyShop</h1>
           <SignUpForm />
           <p className="mt-4 font-semibold">
