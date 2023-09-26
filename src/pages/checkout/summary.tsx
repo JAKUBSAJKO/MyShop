0756c1393c1ec2a0ec1c3007af865769dbd35ae3
JAKUBSAJKO <jakubsajko.inf@gmail.com>
2023-09-26 16:21:29 +0200

    Mobile: adjust summary page style

--- src/pages/checkout/summary.tsx
+++ src/pages/checkout/summary.tsx
@@ -1,19 +1,17 @@
-import { useEffect } from "react";
-import { useRouter } from "next/router";
 import { loadStripe } from "@stripe/stripe-js";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 import { BiArrowBack } from "react-icons/bi";
 import { FaTrashAlt } from "react-icons/fa";
 import { useMutation, useQuery, useQueryClient } from "react-query";
 
+import useLocalStorage from "@/hooks/useLocalStorage";
+import { routes } from "../../../routes/routes";
+import { getProducts, updateQuantity } from "../../../services/services";
 import { useBasketStore } from "../../../stories/store";
 import { Product, ProductInBasket } from "../../../types";
-import { getProducts, updateQuantity } from "../../../services/services";
-import { routes } from "../../../routes/routes";
-import useLocalStorage from "@/hooks/useLocalStorage";
 
-const stripePromise = loadStripe(
-  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string
-);
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string);
 
 export default function Summary() {
   const queryClient = useQueryClient();
@@ -23,10 +21,7 @@ export default function Summary() {
 
   const { data: products } = useQuery("products", getProducts);
 
-  const [basketInLS, setBasketInLS] = useLocalStorage<ProductInBasket[]>(
-    "basketLS",
-    []
-  );
+  const [basketInLS, setBasketInLS] = useLocalStorage<ProductInBasket[]>("basketLS", []);
 
   const { mutate, isLoading } = useMutation(updateQuantity, {
     onSuccess: () => {
@@ -58,20 +53,15 @@ export default function Summary() {
 
   const removeItem = (product: ProductInBasket) => {
     removeFromBasket(product);
-    const productInDB = products.find(
-      (element: Product) => element.id === product.id
-    );
+    const productInDB = products.find((element: Product) => element.id === product.id);
     const currentQuantity = productInDB.quantity + product.quantity;
     mutate({ productId: product.id, currentQuantity });
-    const removeItemInLS = basketInLS.filter(
-      (productInLS) => productInLS.id !== product.id
-    );
+    const removeItemInLS = basketInLS.filter((productInLS) => productInLS.id !== product.id);
     setBasketInLS([...removeItemInLS]);
   };
 
   const total = basket.reduce(
-    (acc, curr) =>
-      curr.quantity > 1 ? curr.price * curr.quantity + acc : curr.price + acc,
+    (acc, curr) => (curr.quantity > 1 ? curr.price * curr.quantity + acc : curr.price + acc),
     0
   );
 
@@ -82,25 +72,19 @@ export default function Summary() {
   }, [basket]);
 
   return (
-    <div className="w-full h-screen p-16 flex bg-orange-400">
-      <div className="basis-3/5 flex flex-col text-white gap-6">
-        <button
-          className="flex items-center gap-2"
-          onClick={() => router.push(routes.home)}
-        >
+    <div className="w-full h-screen p-8 flex flex-col bg-orange-400 md:flex-row md:p-16">
+      <div className="md:basis-3/5 flex flex-col text-white gap-6">
+        <button className="flex items-center gap-2" onClick={() => router.push(routes.home)}>
           <BiArrowBack />
           <p className="uppercase font-medium">Kontynuuj zakupy</p>
         </button>
-        <div className="mt-2">
+        <div className="mb-8 md:mt-2">
           <h1 className="text-4xl">TWÓJ KOSZYK</h1>
           <p>Masz w swoim koszyku {basket.length} rzeczy</p>
         </div>
-        <ul className="max-w-xl flex flex-col gap-4 overflow-y-scroll no-scrollbar">
+        <ul className="hidden max-w-xl md:flex flex-col gap-4 overflow-y-scroll no-scrollbar">
           {basket.map((product) => (
-            <li
-              key={product.id}
-              className="h-24 bg-nav-grey-200 py-4 flex items-center rounded-2xl"
-            >
+            <li key={product.id} className="h-24 bg-nav-grey-200 py-4 flex items-center rounded-2xl">
               <div className="h-16 px-3 mx-4 bg-white flex justify-center items-center rounded-xl">
                 <img src={product.image} alt={product.name} width={48} />
               </div>
@@ -114,13 +98,8 @@ export default function Summary() {
                     </p>
                     <button className="text-lg font-bold">-</button>
                   </div>
-                  <p className="w-12 flex justify-center items-center">
-                    {product.price} zł
-                  </p>
-                  <button
-                    onClick={() => removeItem(product)}
-                    className="hover:text-trash hover:scale-105"
-                  >
+                  <p className="w-12 flex justify-center items-center">{product.price} zł</p>
+                  <button onClick={() => removeItem(product)} className="hover:text-trash hover:scale-105">
                     <FaTrashAlt />
                   </button>
                 </div>
@@ -129,7 +108,7 @@ export default function Summary() {
           ))}
         </ul>
       </div>
-      <div className="basis-2/5 flex flex-col justify-end">
+      <div className="md:basis-2/5 flex flex-col justify-end">
         <div className="max-w-sm bg-nav-grey-200 rounded-2xl text-white p-8 flex flex-col gap-6">
           <h3 className="text-xl">Podsumowanie</h3>
           <hr />
@@ -152,9 +131,7 @@ export default function Summary() {
             role="link"
             onClick={goToCheckout}
             className={`py-3 rounded-lg font-semibold hover:scale-105 ${
-              basket.length < 1
-                ? "bg-gray-400 hover:scale-100"
-                : "bg-orange-500 hover:bg-orange-600"
+              basket.length < 1 ? "bg-gray-400 hover:scale-100" : "bg-orange-500 hover:bg-orange-600"
             }`}
             disabled={basket.length < 1}
           >
