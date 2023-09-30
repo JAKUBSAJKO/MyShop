4e9a9cd116facaf131bd7c11e0a6e3bf9a0f3866
JAKUBSAJKO <jakubsajko.inf@gmail.com>
2023-09-30 20:28:45 +0200

    Mobile: adjust footer style

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="footer footer-center p-4 bg-base-300 text-base-content">
       <div>
-        <p>Copyright © 2023 - All right reserved by MyShop</p>
+        <p className="text-xs sm:text-sm md:text-base">Copyright © 2023 - All right reserved by MyShop</p>
       </div>
     </footer>
   );
