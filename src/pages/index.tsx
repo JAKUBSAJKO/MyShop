84d5df693b4e806ec867b848230e0c4d47bf9ebf
Jakub Sajko <jakub.s@futuresight.com>
2024-02-13 09:15:15 +0100

    Delete redundant console logs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,16 +23,6 @@ export default function Home() {
     if (basketInLS.length > 0) {
       addProductsFromLS(basketInLS);
     }
-
-    // Check to see if this is a redirect back from Checkout
-    const query = new URLSearchParams(window.location.search);
-    if (query.get("success")) {
-      console.log("Order placed! You will receive an email confirmation.");
-    }
-
-    if (query.get("canceled")) {
-      console.log("Order canceled -- continue to shop around and checkout when you’re ready.");
-    }
   }, []);
 
   return (
